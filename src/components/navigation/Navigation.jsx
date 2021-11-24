1ccad25dedb7efbbe4946622a058eab2d9cb5ebb
havc-dev <adrian.curiel.dev@gmail.com>
2021-11-24 00:07:50 -0500

    Update Navigation.jsx

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -46,7 +46,8 @@ const Navigation = () => {
           <Link className="nav-link" to="/about">About</Link>
           <Link className="nav-link" to="/contact">Contact</Link>
           <Link className="nav-link" to="/login">Login</Link>
-          <p>{`${lat}, ${lng}`}</p>
+          <p>{lat}</p>
+          <p>{lng}</p>
         </>}
       <button onClick={getLocation}>Location</button>
      
